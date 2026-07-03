df0c8d26107eb8a3c119112deaf9a098177e6a4d
P34 <amirpea4444@gmail.com>
2026-07-03 01:31:07 +0100

    Inital Tree Commit

---
+++ omni_Infinix-X683B.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from Infinix-X683B device
+$(call inherit-product, device/infinix/Infinix-X683B/device.mk)
+
+PRODUCT_DEVICE := Infinix-X683B
+PRODUCT_NAME := omni_Infinix-X683B
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix X683B
+PRODUCT_MANUFACTURER := infinix
+
+PRODUCT_GMS_CLIENTID_BASE := android-infinix
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vnd_x683b_h694-user 10 QP1A.190711.020 70956 release-keys"
+
+BUILD_FINGERPRINT := Infinix/X683B-OP/Infinix-X683B:10/QP1A.190711.020/210122V130:user/release-keys
